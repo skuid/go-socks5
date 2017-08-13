719cc22b264b44a97b7938ca7d28a56aa2b3e464
Micah Hausler <hausler.m@gmail.com>
2017-08-13 13:40:40 -0400

    fix(metrics): Dropped startup duration metric
    * Added label for client_ip. remote_addr had too high of caridnality to be useful

--- request.go
+++ request.go
@@ -6,7 +6,6 @@ import (
 	"net"
 	"strconv"
 	"strings"
-	"time"
 
 	"golang.org/x/net/context"
 )
@@ -158,10 +157,6 @@ func (s *Server) handleRequest(req *Request, conn conn) error {
 
 // handleConnect is used to handle a connect command
 func (s *Server) handleConnect(ctx context.Context, conn conn, req *Request) error {
-	now := time.Now()
-	defer func() {
-		instrumentRequestSetupDuration(now, string(req.DestAddr.IP))
-	}()
 
 	// Check if this is allowed
 	if ctx_, ok := s.config.Rules.Allow(ctx, req); !ok {
